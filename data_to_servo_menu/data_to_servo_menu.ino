3b311670e2f0c8207dd0421d6e623468dde9a881
johanneshoelker <73756221+johanneshoelker@users.noreply.github.com>
2021-07-07 15:59:06 +0200

    Updated Version from HP PC

--- data_to_servo_menu/data_to_servo_menu.ino
+++ data_to_servo_menu/data_to_servo_menu.ino
@@ -1,5 +1,3 @@
-
-
 #include <Servo.h>
 
 
@@ -21,24 +19,24 @@ int data[7];
 String msg;
 char buffer[27];
 
-int ledPinAC = 24;  //rot
+int ledPinAC = 24;  //grün
 int ledPin90= 28;   //gelb
-int ledPinOff= 32;  //grün
+int ledPinStop= 32;  //rot
 int inPinAC= 22;    // pushbutton connected to digital pin 7
 int inPin90= 26; 
-int inPinOff= 30;
+int inPinStop= 30;
 boolean val_AC= false;      // variable to store the read value
 boolean val_90= false;
-boolean val_Off= true;
+boolean val_Stop= true;
 
 void setup() {
 
   pinMode(ledPinAC, OUTPUT);
   pinMode(ledPin90, OUTPUT);
-  pinMode(ledPinOff, OUTPUT);
+  pinMode(ledPinStop, OUTPUT);
   pinMode(inPinAC, INPUT_PULLUP); 
   pinMode(inPin90, INPUT_PULLUP);
-  pinMode(inPinOff, INPUT_PULLUP);
+  pinMode(inPinStop, INPUT_PULLUP);
   
   Serial.begin(38400);
   Serial.println("<Arduino is ready>");
@@ -50,69 +48,68 @@ void setup() {
   wrist_rot.attach(6);
   wrist_ver.attach(5);
   gripper.attach(3);
+
+  rot_shoulder = 34;
+  angle_arm = 70;
+  rot_upper = 85;
+  angle_forearm = 1;
+  angle_hand = 105;
+  rot_hand = 87;
+  claw_on_off = 4;  //70:zu 30:offen
  
 }
 
 void loop() {
-  angle_down();
-  
-  if(digitalRead(inPinAC)==LOW){
+
+  if(digitalRead(inPinStop)==LOW){
+    val_AC = false;
+    val_90=false;
+    val_Stop=true;
+    delay(10);
+  }
+    
+  else if(digitalRead(inPinAC)==LOW){
     val_AC = true;
-    val_Off=false;
+    val_90=false;
+    val_Stop=false;
     delay(10);
   }
 
-  if(digitalRead(inPin90)==LOW){
+  else if(digitalRead(inPin90)==LOW){
     val_90 = true;
-    val_Off=false;
+    val_AC=false;
+    val_Stop=false;
     delay(10);
   }
-
-  digitalWrite(ledPinOff,val_Off);
+  
+  digitalWrite(ledPinStop,val_Stop);
   digitalWrite(ledPin90,val_90);
   digitalWrite(ledPinAC,val_AC);
-  
+
   if(val_90==true){
-    digitalWrite(ledPinOff,LOW);
-    while(val_Off==LOW){
-      angle_90();
-      if(digitalRead(inPinOff)==LOW){
-        val_Off = true;
-        delay(10);
-      }
-    }
+    angle_90();
   }
 
   else if(val_AC==true){
-    digitalWrite(ledPinOff,LOW);
-    while(val_Off==LOW){
-      recvWithEndMarker();
-      
-      rot_shoulder = data[0];
-      angle_arm = data[1];
-      rot_upper = data[2];
-      angle_forearm = data[3];
-      angle_hand = data[4];
-      rot_hand = data[5];
-      claw_on_off = data[6];
+    recvWithEndMarker();
+
+    rot_shoulder = data[0];
+    angle_arm = data[1];
+    rot_upper = data[2];
+    angle_forearm = data[3];
+    angle_hand = data[4];
+    rot_hand = data[5];
+    claw_on_off = data[6];
+  } 
   
-      base.write(rot_shoulder);
-      shoulder.write(angle_arm);
-      upper_rot.write(rot_upper);
-      elbow.write(angle_forearm);
-      wrist_rot.write(rot_hand);
-      wrist_ver.write(angle_hand);
-      gripper.write(claw_on_off);
-      if(digitalRead(inPinOff)==LOW){
-        val_Off = true;
-        delay(10);
-      }
-    }
-  }
-  val_AC=false;
-  val_90=false;
-  val_Off=true;
-  
+    
+    base.write(rot_shoulder);
+    shoulder.write(angle_arm);
+    upper_rot.write(rot_upper);
+    elbow.write(angle_forearm);
+    wrist_rot.write(rot_hand);
+    wrist_ver.write(angle_hand);
+    gripper.write(claw_on_off);
 }
 
 void recvWithEndMarker() {
@@ -154,41 +151,26 @@ void recvWithEndMarker() {
     data[6] = atoi(buffer);
 
 
-               Serial.print("Data 0:");
-               Serial.println(data[0]);
-               Serial.print(" Data 1:");
-               Serial.println(data[1]);
-               Serial.print(" Data 2:");
-               Serial.println(data[2]);
-               Serial.print(" Data 3:");
-               Serial.println(data[3]);
-               Serial.print(" Data 4:");
-               Serial.println(data[4]);
-               Serial.print(" Data 5:");
-               Serial.println(data[5]);
-               Serial.print("Data 6:");
-               Serial.println(data[6]);
+//               Serial.print("Data 0:");
+//               Serial.println(data[0]);
+//               Serial.print(" Data 1:");
+//               Serial.println(data[1]);
+//               Serial.print(" Data 2:");
+//               Serial.println(data[2]);
+//               Serial.print(" Data 3:");
+//               Serial.println(data[3]);
+//               Serial.print(" Data 4:");
+//               Serial.println(data[4]);
+//               Serial.print(" Data 5:");
+//               Serial.println(data[5]);
+//               Serial.print("Data 6:");
+//               Serial.println(data[6]);
+//               
+    
 
   }
 }
 
-void angle_down(){
-  rot_shoulder = 10;
-  angle_arm = 10;
-  rot_upper = 10;
-  angle_forearm = 10;
-  angle_hand = 10;
-  rot_hand = 10;
-  claw_on_off = 30;  //70:zu 30:offen
-  
-  base.write(rot_shoulder);
-  shoulder.write(angle_arm);
-  upper_rot.write(rot_upper);
-  elbow.write(angle_forearm);
-  wrist_rot.write(rot_hand);
-  wrist_ver.write(angle_hand);
-  gripper.write(claw_on_off);
-}
 
 void angle_90(){
   rot_shoulder = 90;
@@ -198,12 +180,4 @@ void angle_90(){
   angle_hand = 90;
   rot_hand = 90;
   claw_on_off = 70;  //70:zu 30:offen
-  
-  base.write(rot_shoulder);
-  shoulder.write(angle_arm);
-  upper_rot.write(rot_upper);
-  elbow.write(angle_forearm);
-  wrist_rot.write(rot_hand);
-  wrist_ver.write(angle_hand);
-  gripper.write(claw_on_off);
 }
